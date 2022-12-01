a4fe92d26904fe0a17681da05cd344641de95b3c
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-12-01 17:59:11 +0300

    reading&deleting company names

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,15 +1,34 @@
 import { Col, Container, Row } from "react-bootstrap";
-
+import { useDispatch, useSelector } from "react-redux";
+import { RiDeleteBin5Line } from "react-icons/ri";
 const Favourites = () => {
+  const dispatch = useDispatch();
+  const favouriteCompanies = useSelector(
+    (state) => state.favouriteCompanies.content
+  );
   return (
     <Container>
       <Row>
         <Col xs={10} className="mx-auto my-3">
           <h2>FAVOURITE COMPANIES</h2>
         </Col>
-        <Col xs={10} className="mx-auto mb-5">
-          company name
-        </Col>
+        {favouriteCompanies.map((companyName) => (
+          <Col
+            xs={10}
+            className="mx-auto mb-2 d-flex align-items-center justify-content-between"
+          >
+            <div>{companyName}</div>
+            <RiDeleteBin5Line
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                dispatch({
+                  type: "REMOVE_FROM_FAVOURITES",
+                  payload: companyName,
+                });
+              }}
+            />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
